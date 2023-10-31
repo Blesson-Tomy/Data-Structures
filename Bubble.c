b4e95c6b09c8a0dbe6dc2961bd7c5c7b71d87142
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2023-10-31 18:01:38 +0530

    update programs

---
+++ Bubble.c
@@ -0,0 +1,53 @@
+/**********************************************************************************
+ * File             :   Bubble.c
+ * Program          :   Program to perform bubble search
+ * Language         :   C Language
+ * Author           :   Blesson Karikulammalayil Tomy
+ * Class	    :	CSE B
+ * Roll No	    :	22
+ * Date             :   30/09/2023
+***********************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+	int i,j,n,temp,c=0;
+	c++;
+	printf("Enter the number of elements:\n");
+	scanf("%d",&n);
+	
+	int arr[n];
+	
+	printf("Enter the elements:\n");
+	for(i=0;i<n;i++)
+	{ c++;
+	c++;
+		scanf("%d", &arr[i]);
+	}c++;
+	
+	for(i=0;i<n-1;i++)
+	{ c++;
+		for(j=0;j<n-1-i;j++)
+		{ c++;
+		c++;
+			if(arr[j]>arr[j+1])
+			{ c++;
+				temp=arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+			}
+		}c++;
+	}c++;
+	
+	printf("The sorted array is: ");
+	for(i=0;i<n;i++)
+	{c++;
+	c++;
+		printf("%d ",arr[i]);
+	}c++;
+	
+	printf("\nTime Complexity is %d.", c);
+printf("\nThe Space Complexity is %d.\n", 24+4*n);
+return 0;
+	
+}
---
+++ Selection.c
@@ -0,0 +1,59 @@
+/**********************************************************************************
+ * File             :   Selection.c
+ * Program          :   Program to perform selection sort
+ * Language         :   C Language
+ * Author           :   Blesson Karikulammalayil Tomy
+ * Class	    :	CSE B
+ * Roll No	    :	22
+ * Date             :   30/09/2023
+***********************************************************************************/
+#include <stdio.h>
+
+int main() 
+{
+int n, i, j, temp,small,c=0;
+c++;
+printf("Enter the array size: ");
+scanf("%d", &n);
+
+int a[n];
+printf("Enter the  elements: \n");
+for (i = 0; i < n; i++) 
+{	c++;
+	c++;
+	scanf("%d", &a[i]);
+}c++;
+j=i+1;
+for(i=0;i<n-1;i++)
+{ c++;
+	small=i;
+		for(j=i+1;j<n;j++)
+		{ c++;
+			c++;
+			if(a[j]<a[small])
+			{ c++;
+				small=j;
+			}
+		}c++;
+
+	if(i!=small)
+	{ c++;
+		temp=a[i];
+		a[i]=a[small];
+		a[small]=temp;
+	}
+		
+}c++;
+
+printf("The sorted array is: ");
+for (i = 0; i < n; i++) 
+{	c++;
+	c++;
+	printf("%d ", a[i]);
+}c++;
+
+printf("\nTime Complexity is %d.", c);
+printf("\nThe Space Complexity is %d.\n", 24+4*n);
+return 0;
+}	
+	
